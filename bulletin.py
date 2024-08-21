2a9887a84b8455ddfebac2eb0e9b373e0a482fcb
drod75 <dr507498@gmail.com>
2024-08-21 16:22:37 -0400

    'edits'

--- bulletin.py
+++ bulletin.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title('CUNY Essentials Bulletin')
-st.text('Welcome to the CUNY Essentials Bulletin, here you will find the resources needed to contact help and document your feelings.')
+st.text('Welcome to the CUNY Essentials Bulletin! \nHere you will find the resources needed to contact help and document your feelings!')
 st.text('In this site there are two main pages:')
-st.markdown('* Journal where you can document how you feel')
-st.markdown('* Essentials where you can find the many mental health services CUNY and the government provides')
-st.text('With this we hope you make full use of our site and get any help you need, there are people always here for you!')
+st.markdown('* __Journal__ where you can document how you feel')
+st.markdown('* __Essentials__ where you can find the many mental health services CUNY and the government provides')
+st.text('With this we hope you make full use of our site and get any help you need, \nthere are people always here for you!')
