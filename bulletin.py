451ce88e85a7428c2d1f51acbfdaffd6a7d3a8fa
Esteban Mesa <112445347+Mesa112@users.noreply.github.com>
2024-08-21 17:23:15 -0400

    Update bulletin.py

--- bulletin.py
+++ bulletin.py
@@ -10,3 +10,4 @@ st.text('In this site there are two main pages:')
 st.markdown('* __Journal__ where you can document how you feel')
 st.markdown('* __Essentials__ where you can find the many mental health services CUNY and the government provides')
 st.text('With this we hope you make full use of our site and get any help you need, \nthere are people always here for you!')
+#just makeing sure this works
