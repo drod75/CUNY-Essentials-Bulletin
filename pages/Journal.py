bc15ff49d7d484d16377827913023f6c35d86ce2
drod75 <dr507498@gmail.com>
2024-08-21 20:32:21 -0400

    'login bugfixes'

--- pages/Journal.py
+++ pages/Journal.py
@@ -17,7 +17,10 @@ authenticator = stauth.Authenticate(
     config['pre-authorized']
 )
 
+# login widget
 authenticator.login()
+
+#authenticating
 if st.session_state['authentication_status']:
     authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
@@ -27,46 +30,59 @@ elif st.session_state['authentication_status'] is False:
 elif st.session_state['authentication_status'] is None:
     st.warning('Please enter your username and password')
 
+# reset password widget
 if st.session_state['authentication_status']:
     try:
         if authenticator.reset_password(st.session_state['username']):
             st.success('Password modified successfully')
+            with open('../config.yaml', 'w') as file:
+                yaml.dump(config, file, default_flow_style=False)
     except Exception as e:
         st.error(e)
 
+#  new user
 try:
     email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
     if email_of_registered_user:
         st.success('User registered successfully')
+        with open('../config.yaml', 'w') as file:
+            yaml.dump(config, file, default_flow_style=False)
 except Exception as e:
     st.error(e)
 
+# forgor password
 try:
     username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
     if username_of_forgotten_password:
         st.success('New password to be sent securely')
         # The developer should securely transfer the new password to the user.
+        with open('../config.yaml', 'w') as file:
+            yaml.dump(config, file, default_flow_style=False)
     elif username_of_forgotten_password == False:
         st.error('Username not found')
 except Exception as e:
     st.error(e)
 
+# forgor username
 try:
     username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
     if username_of_forgotten_username:
         st.success('Username to be sent securely')
         # The developer should securely transfer the username to the user.
+        with open('../config.yaml', 'w') as file:
+            yaml.dump(config, file, default_flow_style=False)
     elif username_of_forgotten_username == False:
         st.error('Email not found')
 except Exception as e:
     st.error(e)
 
+# update user details
 if st.session_state['authentication_status']:
     try:
         if authenticator.update_user_details(st.session_state['username']):
             st.success('Entries updated successfully')
     except Exception as e:
         st.error(e)
+    with open('../config.yaml', 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
 
-with open('../config.yaml', 'w') as file:
-    yaml.dump(config, file, default_flow_style=False)
