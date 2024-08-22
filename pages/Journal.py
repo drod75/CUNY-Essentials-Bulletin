f1119f6d0dcc3b7032b7698f77770623145c9777
David Rodriguez <139922486+drod75@users.noreply.github.com>
2024-08-22 00:52:48 +0000

    'edits'

--- pages/Journal.py
+++ pages/Journal.py
@@ -3,7 +3,7 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-with open(r'gh spaces\CUNY-Essentials-Bulletin\pages\config.yaml') as file:
+with open('gh spaces\CUNY-Essentials-Bulletin\pages\config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Pre-hashing all plain text passwords once
@@ -35,7 +35,7 @@ if st.session_state['authentication_status']:
     try:
         if authenticator.reset_password(st.session_state['username']):
             st.success('Password modified successfully')
-            with open(r'config.yaml', 'w') as file:
+            with open('config.yaml', 'w') as file:
                 yaml.dump(config, file, default_flow_style=False)
     except Exception as e:
         st.error(e)
@@ -45,7 +45,7 @@ try:
     email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
     if email_of_registered_user:
         st.success('User registered successfully')
-        with open(r'config.yaml', 'w') as file:
+        with open('config.yaml', 'w') as file:
             yaml.dump(config, file, default_flow_style=False)
 except Exception as e:
     st.error(e)
@@ -56,7 +56,7 @@ try:
     if username_of_forgotten_password:
         st.success('New password to be sent securely')
         # The developer should securely transfer the new password to the user.
-        with open(r'config.yaml', 'w') as file:
+        with open('config.yaml', 'w') as file:
             yaml.dump(config, file, default_flow_style=False)
     elif username_of_forgotten_password == False:
         st.error('Username not found')
@@ -69,7 +69,7 @@ try:
     if username_of_forgotten_username:
         st.success('Username to be sent securely')
         # The developer should securely transfer the username to the user.
-        with open(r'config.yaml', 'w') as file:
+        with open('config.yaml', 'w') as file:
             yaml.dump(config, file, default_flow_style=False)
     elif username_of_forgotten_username == False:
         st.error('Email not found')
@@ -83,6 +83,6 @@ if st.session_state['authentication_status']:
             st.success('Entries updated successfully')
     except Exception as e:
         st.error(e)
-    with open(r'config.yaml', 'w') as file:
+    with open('config.yaml', 'w') as file:
         yaml.dump(config, file, default_flow_style=False)
 
