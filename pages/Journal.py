0d91bdb55ef3b74dac307a00d39b5e7b76ec9c17
drod75 <dr507498@gmail.com>
2024-08-22 11:48:51 -0400

    'journal edit'

--- pages/Journal.py
+++ pages/Journal.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 #access text file step 1: account checking
 if not(st.session_state.get('authentication_status')):
-    st.text('# Please Login to use this feature')
+    st.markdown('# Please Login to use this feature')
 else:
     st.text('WIP')
