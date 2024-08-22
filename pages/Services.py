2004ae0f23f6a1917e9dce64de2704f3ee2955cf
drod75 <dr507498@gmail.com>
2024-08-22 13:15:31 -0400

    'services edits'

--- pages/Services.py
+++ pages/Services.py
@@ -1,1 +1,60 @@
-import streamlit as sts
+import streamlit as st
+
+# Insert containers separated into tabs:
+tab1, tab2, tab3 = st.tabs(["Community Colleges", "Bachelors Colleges", "Master Colleges"])
+
+#tab 1 writing
+tab1.markdown('--- \n#### Bronx Community College')
+
+tab1.markdown('--- \n#### Hostos Community College')
+
+tab1.markdown('--- \n#### Kingsborough Community College')
+
+tab1.markdown('--- \n#### Borough of Manhattan Community College')
+
+tab1.markdown('--- \n#### Guttman Community College')
+
+tab1.markdown('--- \n#### LaGuardia Community College')
+
+tab1.markdown('--- \n#### Queensborough Community College')
+tab1.markdown('---')
+
+#tab 2 writing
+tab2.markdown('--- \n#### Lehman College')
+
+tab2.markdown('--- \n#### Brooklyn College')
+
+tab2.markdown('--- \n#### Medgar Evans College')
+
+tab2.markdown('--- \n#### Baruch College')
+
+tab2.markdown('--- \n#### City Tech College')
+
+tab2.markdown('--- \n#### City College of New York')
+
+tab2.markdown('--- \n#### Hunter College')
+
+tab2.markdown('--- \n#### John Jay College')
+
+tab2.markdown('--- \n#### Macaulay Honors College')
+
+tab2.markdown('--- \n#### Queens College')
+
+tab2.markdown('--- \n#### York College')
+
+tab2.markdown('--- \n#### College of Staten Island')
+tab2.markdown('---')
+
+#tab 3 writing
+tab3.markdown('--- \n#### Craig Newmark Graduate School of Journalism')
+
+tab3.markdown('--- \n#### CUNY Graduate Center')
+
+tab3.markdown('--- \n#### Graduate School of Public Health & Health Policy')
+
+tab3.markdown('--- \n#### School of Labor and Urban Studies')
+
+tab3.markdown('--- \n#### School of Professional Studies')
+
+tab3.markdown('--- \n#### School of Law')
+tab3.markdown('---')
