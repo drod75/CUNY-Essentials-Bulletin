6fe989dcac03af22b814b0a4cf6043cf10a0ef15
drod75 <dr507498@gmail.com>
2024-08-23 15:41:44 -0400

    'journal edits'

--- pages/Journal.py
+++ pages/Journal.py
@@ -9,9 +9,6 @@ else:
     st.set_page_config(layout="wide")
     if 'journal' not in st.session_state:
         st.session_state.journal = ""
-    journal_csv = pd.read_csv('journal_data.csv')
-    account_active = journal_csv.loc[(journal_csv['name'] == st.session_state['name']) & (journal_csv['username'] == st.session_state['username'])]
-    st.session_state.journal = account_active['journal_string']
     user_input = st.text_area("Write your Journal below", height=500)
 
     if st.button("Save Journal"):
@@ -23,7 +20,14 @@ else:
     # Show the saved journal content
     st.write("### Saved Journal:")
     text = st.empty()
-    st.write(st.session_state.journal)
+    
+    journal_csv = pd.read_csv('journal_data.csv')
+    account_active = journal_csv.loc[(journal_csv['name'] == st.session_state['name']) & (journal_csv['username'] == st.session_state['username'])]
+    account_active['journal_string'] += st.session_state.journal
+    
+    st.write(account_active['journal_String'])
+
+    
 
     account_active = account_active[['name','username','journal_string']]
     journal_csv.update(account_active,overwrite=True)
