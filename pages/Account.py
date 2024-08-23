3d641877150724fc8778faebd9e6466166ff83f8
Deep Patel <80269801+DPatel03@users.noreply.github.com>
2024-08-23 14:32:24 -0400

    'third-commit'

--- pages/Account.py
+++ pages/Account.py
@@ -2,60 +2,70 @@ import streamlit as st
 from authentication import authenticate_user, register_user
 import yaml
 
+
 st.set_page_config(layout='wide')
+
 
 # Authenticate user
 authenticator, config, config_path = authenticate_user()
 
-# Handle authentication status
-if st.session_state.get('authentication_status'):
-    authenticator.logout()
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
 
-    # Password reset functionality
-    try:
-        if authenticator.reset_password(st.session_state['username']):
-            st.success('Password modified successfully')
-    except Exception as e:
-        st.error(e)
+# Handle user login or registration options
+if not st.session_state.get('authentication_status'):
+   st.warning('Please log in to access this content.')
 
-    # User registration
-    register_user(config, config_path)
 
-    # Forgot password functionality
-    try:
-        username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
-        if username_of_forgotten_password:
-            st.success('New password to be sent securely')
-        elif username_of_forgotten_password is False:
-            st.error('Username not found')
-    except Exception as e:
-        st.error(e)
+   # Forgot password functionality
+   try:
+       username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+       if username_of_forgotten_password:
+           st.success('New password to be sent securely')
+       elif username_of_forgotten_password is False:
+           st.error('Username not found')
+   except Exception as e:
+       st.error(e)
 
-    # Forgot username functionality
-    try:
-        username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
-        if username_of_forgotten_username:
-            st.success('Username to be sent securely')
-        elif username_of_forgotten_username is False:
-            st.error('Email not found')
-    except Exception as e:
-        st.error(e)
 
-    # Update user details
-    try:
-        if authenticator.update_user_details(st.session_state['username']):
-            st.success('Entries updated successfully')
-    except Exception as e:
-        st.error(e)
+   # Forgot username functionality
+   try:
+       username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
+       if username_of_forgotten_username:
+           st.success('Username to be sent securely')
+       elif username_of_forgotten_username is False:
+           st.error('Email not found')
+   except Exception as e:
+       st.error(e)
 
-    # Save the updated configuration
-    with open(config_path, 'w') as file:
-        yaml.dump(config, file, default_flow_style=False)
+
+   # Add a "Register" section
+   st.subheader("Don't have an account? Register here!")
+   register_user(config, config_path)
+
+
 else:
-    st.warning('Please log in to access this content.')
-    
-    # Add a "Register" section
-    st.subheader("Don't have an account? Register here!")
-    register_user(config, config_path)
+   authenticator.logout()
+   st.write(f'Welcome *{st.session_state["name"]}*')
+   st.title('Some content')
+
+
+   # Password reset functionality
+   try:
+       if authenticator.reset_password(st.session_state['username']):
+           st.success('Password modified successfully')
+   except Exception as e:
+       st.error(e)
+
+
+   # Update user details
+   try:
+       if authenticator.update_user_details(st.session_state['username']):
+           st.success('Entries updated successfully')
+   except Exception as e:
+       st.error(e)
+
+
+   # Save the updated configuration
+   with open(config_path, 'w') as file:
+       yaml.dump(config, file, default_flow_style=False)
+
+
--- pages/Journal.py
+++ pages/Journal.py
@@ -5,4 +5,19 @@ from streamlit_ace import st_ace, KEYBINDINGS, LANGUAGES, THEMES
 if not(st.session_state.get('authentication_status')):
     st.markdown('# Please Login to use this feature')
 else:
-    st.text('WIP')
+    st.set_page_config(layout="wide")
+    if 'journal' not in st.session_state:
+        st.session_state.journal = ""
+    user_input = st.text_area("Write your Journal below", height=500)
+
+    if st.button("Save Journal"):
+        if user_input:
+            st.session_state.journal += f"{user_input}\n\n" 
+            st.success("Journal saved successfully!")
+        else:
+            st.warning("You have not written anything to save.")
+    # Show the saved journal content
+    st.write("### Saved Journal:")
+    text = st.empty()
+    st.write(st.session_state.journal)
+
